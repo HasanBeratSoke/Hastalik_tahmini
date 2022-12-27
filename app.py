bf11645c0fbeebeaf1dea0a32cb6cd8aafbe029c
HasanBeratSoke <hasanberatsoke@hotmail.com>
2022-12-27 16:19:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ pickle_in = open('knn.pkl', 'rb')
 classifier = pickle.load(pickle_in)
 
 
-st.title('Diabetes Prediction')
+st.title('Diyabet Hastalığı Tahmini')
 name = st.text_input("İsim:")
 pregnancy = st.number_input("Hamile kalma sayısı:", min_value=0)
 glucose = st.number_input("Plazma Glikoz Konsantrasyonu:")
@@ -23,11 +23,11 @@ skin = st.number_input("Triceps cilt kıvrım kalınlığı (mm):")
 insulin = st.number_input("2 saatlik serum insülini (mu U/ml):")
 bmi = st.number_input("Vücut kitle indeksi (kg cinsinden ağırlık/(m cinsinden yükseklik)^2):")
 dpf = st.number_input("Diyabet Soyağacı İşlevi:")
-age = st.number_input("Yaşss:")
+age = st.number_input("Yaş:")
 submit = st.button('Tahmin')
 if submit:
     prediction = classifier.predict([[pregnancy, glucose, bp, skin, insulin, bmi, dpf, age]])
     if prediction == 0:
-        st.write('Tebrikler',name,'Diabet hastası değilsiniz.')
+        st.write('Merhaba',name,',şuanda şeker hastası olma ihtimaliniz düşük ama dikkatli olmada fayda var.')
     else:
-        st.write(name," diabet hastalığı olma ihtimaliniz yüksek, lütfen en yalın zamanda bir doktara gözükünüz.")
+        st.write(name," Şeker Hastası olma ihtimaliniz yüksek, lütfen en yakın zamanda bir doktara görükmeniz gerekmektedir.")
