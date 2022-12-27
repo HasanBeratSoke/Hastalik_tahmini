8b4f44a58ddd1bf4b6e7c934a28c0b1a8868c36e
HasanBeratSoke <hasanberatsoke@hotmail.com>
2022-12-27 15:55:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,4 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import plotly.express as px
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import StandardScaler
 import pickle
 
 
