b453706a8f2a6543eddf18f8657c5d916a66260a
HasanBeratSoke <hasanberatsoke@hotmail.com>
2022-12-27 16:00:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
 import pickle
+
 
 
 
